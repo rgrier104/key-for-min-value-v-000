ebd37a34b96ff62aea1255a422a791cce11b2e90
Rachel Grier <rgrier104@gmail.com>
2018-09-07 03:50:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,12 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-  min_val = name_hash[0]
-  name_hash.collect do |keys, value|
+  array = []
+  name_has.each do |keys, value|
+    array << value
+  end
+  min_val = array[0]
+  array.each do |value|
     if value < min_val
        min_val = value
     end
